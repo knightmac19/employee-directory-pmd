feba3753b310b55800b33bf962f09f9a15694a39
Patrick Dunn <pmdunn78@gmail.com>
2020-04-07 10:20:45 -0400

    adding header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Directory from "./pages/Directory";
-
+import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
 import Footer from "./components/Footer";
 
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <div>
-        
+        <Header />
         <Wrapper>
           <Route exact path="/" component={Directory} />
         </Wrapper>
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+
+function Header() {
+  return (
+    <div>
+      <h1 className="text-center">Employee Directory</h1>
+    </div>
+  );
+}
+
+export default Header;
