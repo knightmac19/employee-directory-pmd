5750cb7576bba760e0509516f01bfdc6fd6bb30d
Patrick Dunn <pmdunn78@gmail.com>
2020-04-07 10:16:48 -0400

    fix footer, rm hero imgs

--- src/pages/Directory.js
+++ src/pages/Directory.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import Hero from "../components/Hero";
+
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
@@ -29,9 +29,7 @@ class Directory extends Component {
     return (
       <div>
         <hr/>
-        <Hero backgroundImage="./">
-          <h1>Meet Our Team</h1>
-        </Hero>
+        
         <Container style={{ marginTop: 30 }}>
           <Row>
             <Col size="md-12">
