2de85483d65b8730b440ad867a35870b711c7f8f
Patrick Dunn <pmdunn78@gmail.com>
2020-04-07 10:35:56 -0400

    formatting Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,9 @@ import "./style.css";
 
 function Header() {
   return (
-    <div>
-      <h1 className="text-center">Employee Directory</h1>
+    <div className="jumbotron text-center">
+      <h1 >Employee Directory</h1>
+      <p >Sort by heading or use the search bar to narrow your results</p>
     </div>
   );
 }
