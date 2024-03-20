c9ccf0338d6d3533d9912ff666c470516fd6882a
Patrick Dunn <pmdunn78@gmail.com>
2024-03-20 15:27:17 -0400

    fixing chevron icon

--- src/components/EmployeeList/index.js
+++ src/components/EmployeeList/index.js
@@ -6,8 +6,8 @@ const sortTypes = {
   up: {
     name: {
       class: "up",
-      sortFn: arr => {
-        let sortedArr = arr.sort((a,b) => {
+      sortFn: (arr) => {
+        let sortedArr = arr.sort((a, b) => {
           var nameA = a.name.toUpperCase();
           var nameB = b.name.toUpperCase();
           if (nameA < nameB) {
@@ -19,15 +19,14 @@ const sortTypes = {
           return 0;
         });
         return sortedArr;
-      }
+      },
     },
-
   },
   down: {
     name: {
       class: "down",
-      sortFn: arr => {
-        let sortedArr = arr.sort((a,b) => {
+      sortFn: (arr) => {
+        let sortedArr = arr.sort((a, b) => {
           var nameA = a.name.toUpperCase();
           var nameB = b.name.toUpperCase();
           if (nameA < nameB) {
@@ -39,12 +38,10 @@ const sortTypes = {
           return 0;
         });
         return sortedArr;
-      }
+      },
     },
-
-  }
-}
-
+  },
+};
 
 function EmployeeList(props) {
   return (
@@ -56,7 +53,11 @@ function EmployeeList(props) {
             Name
             <span> </span>
             <button onClick={props.onSortChange}>
-              <i className={`fas fa-chevron-${sortTypes[props.currentSort].name.class}`} />
+              <i
+                className={`fas fa-chevron-${
+                  sortTypes[props.currentSort].name.class
+                }`}
+              />
             </button>
           </th>
           <th scope="col">Phone</th>
@@ -65,61 +66,43 @@ function EmployeeList(props) {
         </tr>
       </thead>
       <tbody>
-        {
-          (props.search === "")
-          ? sortTypes[props.currentSort].name.sortFn(props.employees)
-          
-          .map(employee => (
-            <tr key={employee.id}>
-              <td>
-                <img alt="IMG" 
-                src={employee.thumbnail} 
-                className="img" />
-              </td>
-              <td>
-                {employee.name}
-              </td>
-              <td>
-                {employee.phone}
-              </td>
-              <td type="email">
-                <Link to={employee.email}>
-                  {employee.email}
-                </Link>
-              </td>
-              <td>
-                {employee.dob}
-              </td>
-            </tr>
-          ))
-          : sortTypes[props.currentSort].name.sortFn(props.employees)
-          .filter(employee => employee.meta
-          .includes(props.search))
-          .map(filteredEmp => (
-            <tr key={filteredEmp.id}>
-              <td>
-                <img alt="IMG" 
-                  src={filteredEmp.thumbnail} 
-                  className="img" 
-                />
-              </td>
-              <td>
-                {filteredEmp.name}
-              </td>
-              <td>
-                {filteredEmp.phone}
-              </td>
-              <td type="email">
-                <Link to={filteredEmp.email}>
-                  {filteredEmp.email}
-                </Link>
-              </td>
-              <td>
-                {filteredEmp.dob}
-              </td>
-            </tr>
-          ))
-        }
+        {props.search === ""
+          ? sortTypes[props.currentSort].name
+              .sortFn(props.employees)
+
+              .map((employee) => (
+                <tr key={employee.id}>
+                  <td>
+                    <img alt="IMG" src={employee.thumbnail} className="img" />
+                  </td>
+                  <td>{employee.name}</td>
+                  <td>{employee.phone}</td>
+                  <td type="email">
+                    <Link to={employee.email}>{employee.email}</Link>
+                  </td>
+                  <td>{employee.dob}</td>
+                </tr>
+              ))
+          : sortTypes[props.currentSort].name
+              .sortFn(props.employees)
+              .filter((employee) => employee.meta.includes(props.search))
+              .map((filteredEmp) => (
+                <tr key={filteredEmp.id}>
+                  <td>
+                    <img
+                      alt="IMG"
+                      src={filteredEmp.thumbnail}
+                      className="img"
+                    />
+                  </td>
+                  <td>{filteredEmp.name}</td>
+                  <td>{filteredEmp.phone}</td>
+                  <td type="email">
+                    <Link to={filteredEmp.email}>{filteredEmp.email}</Link>
+                  </td>
+                  <td>{filteredEmp.dob}</td>
+                </tr>
+              ))}
       </tbody>
     </table>
   );
